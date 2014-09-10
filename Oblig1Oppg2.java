f2d5d5c479500a7423836b4871290fafef4e0dc5
4bZurd <doggen75@gmail.com>
2014-09-10 14:01:27 +0200

    Update Oblig1Oppg2.java

--- Oblig1Oppg2.java
+++ Oblig1Oppg2.java
@@ -1,13 +1,3 @@
-//Oppgave 2
-
-//Lag et applikasjonsprogram som beregner og skriver ut summen av alle tall mellom to grenser som brukeren skriver inn.
-//Programmet skal starte med å lese inn nedre og øvre grense for summen. Dersom innlest øvre grense er mindre enn eller lik nedre grense,
-//skal programmet skrive ut en melding til brukeren om dette og foreta ny innlesing. Når godkjente grenser er lest inn,
-//skal programmet beregne nevnte sum og summen skal så skrive den ut som en sum. Hvis f. eks. nedre grense er 2 og øvre grense er 8 skal det skrives
-//ut følgende tekst: "2 + 3 + 4 + 5 + 6 + 7 + 8 = 35".
-//Test ut programmet med nedre grense lik 1 og øvre grense lik 100. Legg inn et linjeskift for hvert 10. tall i summen.
-
-
 import javax.swing.JOptionPane;//importerer JOptionPane.
 
 public class Obligoppgave2
@@ -32,25 +22,12 @@ public class Obligoppgave2
 		x = Integer.parseInt( nedregrense );
 		y = Integer.parseInt( øvregrense );
 
+		if ( y <= x )
+			JOptionPane.showMessageDialog ( "Du må skrive inn et tall som er høyere enn " + x + "!", "Tallet er for lavt", JOptionPane.ERROR_MESSAGE )
+		else if ( y > x )
 
-		while( x <= 100 )
-		{
-			sum = sum + x;
-			x = x + 1;
-		}
+		// vi trenger å definere hva som skjer etter at den har etablert om y er mindre enn eller lik x
+		// vi må få den til å gå tilbake for å skrive inn nytt tall for øvre grense hvis ikke y er større enn x
 
-
-
-
-
-
-
-
-
-
-
-		JOptionPane.showMessageDialog(null,
-		"Summen av tallene fra 1 til 100 er " + sum);
 	}
-
 }
