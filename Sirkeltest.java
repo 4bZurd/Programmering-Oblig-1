d33d5688b1e74e52d2515bf5e42cf3374cc01984
scarecrow80 <scarecrow80@users.noreply.github.com>
2014-09-12 12:46:04 +0200

    Update Sirkeltest.java

--- Sirkeltest.java
+++ Sirkeltest.java
@@ -29,51 +29,53 @@ public class SirkelTest
 */
 
 import javax.swing.JOptionPane; //importerer JOptionPane for å kunne benytte utskrift
+import java.text.DecimalFormat; //importerer decimalformatet
 
-public class Sirkeltest
+public class Sirkeltest	// Navngi klassen
 {
 
     public static void main(String [] args)
 	{
-		double r = 0.00;
-		double r2 =0.00;
+		double r = 0.00;					// Deklarerer variablene som skal brukes i programmet
+		double r2 = 0.00;
         Sirkel sirkel;
         String utskrift = "";
         int gjennoms = 0;
 
 
-		while ( r >= 0)
+		while ( r >= 0 )		//Utføres dersom det blir lest inn en positiv radius.
 		{
 			String tom = JOptionPane.showInputDialog("Sett en radius for skirkelen. Avslutt programmet med et negativt tall.");
-			if(tom.isEmpty())
+
+			if(tom.isEmpty())	// Lar programmet vite at det ikke er lest inn en verdi.
 			{
-				JOptionPane.showMessageDialog(null,"Du har ikke lest inn noen verdi!");
+				JOptionPane.showMessageDialog(null,"Du har ikke lest inn noen verdi!");	// Dialogen som kommer dersom du ikke leser inn noen verdi, bare starter programmet.
 			}
-			else
+			else		// Utføres når det blir lest inn en positiv verdi
 			{
 				r = Double.parseDouble(tom);
-				r2+=r;
-				if (r >=0)
+				r2 += r;
+				if (r >= 0)
 				{
-				sirkel = new Sirkel(r);
+				sirkel = new Sirkel( r );
 				utskrift = sirkel.vis();
-				JOptionPane.showMessageDialog(null,utskrift);
+				JOptionPane.showMessageDialog( null, utskrift );
 				gjennoms ++;
 		    }
 		    }
 		}
 
+	DecimalFormat d = new DecimalFormat("0.00");		// For at vi skal sikre oss bare 2 desimaler i utskriften på gjennomsnittet av radiusene.
+    double r1 = r2 / gjennoms;							// Utregningen for gjennomsnittet på radiusene som er lest inn.
 
-    double r1 = r2 / gjennoms;
-
-    if(r1<0)
+    if( r1 < 0 )	// Utføres dersom det blir lest inn en negativ verdi i starten av programmet.
     {
-		JOptionPane.showMessageDialog(null,"Du har ikke lest inn positiv radius!");
+		JOptionPane.showMessageDialog(null,"Du har ikke lest inn positiv radius!", "Feil", JOptionPane.ERROR_MESSAGE);	// Dialogen som kommer dersom det blir lest inn en negativ verdi i starten av programmet.
 	}
-     else
+     else		// Utføres når programmet blir avsluttet etter at positive verdier har blir lest inn.
     {
 
-    JOptionPane.showMessageDialog(null, "Gjennomsnittet på" + r2 + " / " + gjennoms + " = " + r1);
+    JOptionPane.showMessageDialog(null, "Gjennomsnittet på radiusene som er lest inn \n" + r2 + " / " + gjennoms + " = " + d.format(r1));	// Utskriften på gjennomsnittet som kommer når programmet avsluttes.
     }
   }
 }
